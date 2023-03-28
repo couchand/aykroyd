3d41bdaf9498f1c678170f547ec92d8bd1eed9ad
Andrew Dona-Couch <hi@andrewcou.ch>
2023-03-28 15:44:05 -0400

    handle errors in tests

--- akroyd-test-async/src/bin/async.rs
+++ akroyd-test-async/src/bin/async.rs
@@ -1,7 +1,30 @@
 use akroyd::*;
 use akroyd_test::*;
 
-async fn async_main() {
+async fn run_test(client: &tokio_postgres::Client) -> Result<(), tokio_postgres::Error> {
+    println!("Querying all customers...");
+    for customer in client.run(&GetCustomers).await? {
+        println!("Got customer: {:?}", customer);
+    }
+
+    println!("Querying all customers another way...");
+    for customer in client.run(&GetCustomers2).await? {
+        println!("Got customer: {:?}", customer);
+    }
+
+    println!("Searching for customers with name ending 'm'...");
+    for customer in client.run(&SearchCustomersByName("%m")).await? {
+        println!("Got customer: {:?}", customer);
+    }
+
+    println!("Getting customer by id 1...");
+    let customer = client.run_one(&GetCustomer::by_id(1)).await?;
+    println!("Got customer: {:?}", customer);
+
+    Ok(())
+}
+
+async fn async_main() -> bool {
     use tokio_postgres::NoTls;
     let (client, worker) = tokio_postgres::connect(
         "host=localhost user=akroyd_test password=akroyd_test",
@@ -19,32 +42,23 @@ async fn async_main() {
     println!("Creating table & inserting data...");
     client.batch_execute("CREATE TABLE customers (id SERIAL PRIMARY KEY, name TEXT); INSERT INTO customers (id, name) VALUES (1, 'Jan'), (2, 'Tim');").await.expect("setup");
 
-    println!("Querying all customers...");
-    for customer in client.run(&GetCustomers).await.expect("query") {
-        println!("Got customer: {:?}", customer);
-    }
-
-    println!("Querying all customers another way...");
-    for customer in client.run(&GetCustomers2).await.expect("query") {
-        println!("Got customer: {:?}", customer);
-    }
-
-    println!("Searching for customers with name ending 'm'...");
-    for customer in client.run(&SearchCustomersByName("%m")).await.expect("query") {
-        println!("Got customer: {:?}", customer);
-    }
-
-    println!("Getting customer by id 1...");
-    let customer = client.run_one(&GetCustomer::by_id(1)).await.expect("query");
-    println!("Got customer: {:?}", customer);
+    let ok = match run_test(&client).await {
+        Ok(_) => true,
+        Err(e) => {
+            eprintln!("Error: {e}");
+            false
+        }
+    };
 
     println!("Dropping table...");
     client.batch_execute("DROP TABLE customers;").await.expect("setup");
+
+    ok
 }
 
 #[tokio::main]
 async fn main() {
     println!("testing asynchronous client...");
-    async_main().await;
-    println!("Done.");
+    let ok = async_main().await;
+    println!("Done{}.", if ok { "" } else { " (with errors)" });
 }
--- akroyd-test-sync/src/bin/sync.rs
+++ akroyd-test-sync/src/bin/sync.rs
@@ -1,7 +1,30 @@
 use akroyd::*;
 use akroyd_test::*;
 
-fn sync_main() {
+fn run_test(client: &mut postgres::Client) -> Result<(), postgres::Error> {
+    println!("Querying all customers...");
+    for customer in client.run(&GetCustomers)? {
+        println!("Got customer: {:?}", customer);
+    }
+
+    println!("Querying all customers another way...");
+    for customer in client.run(&GetCustomers2)? {
+        println!("Got customer: {:?}", customer);
+    }
+
+    println!("Searching for customers with name ending 'm'...");
+    for customer in client.run(&SearchCustomersByName("%m"))? {
+        println!("Got customer: {:?}", customer);
+    }
+
+    println!("Getting customer by id 1...");
+    let customer = client.run_one(&GetCustomer::by_id(1))?;
+    println!("Got customer: {:?}", customer);
+
+    Ok(())
+}
+
+fn sync_main() -> bool {
     use postgres::{Client, NoTls};
 
     let mut client = Client::connect(
@@ -13,31 +36,22 @@ fn sync_main() {
     println!("Creating table & inserting data...");
     client.batch_execute("CREATE TABLE customers (id SERIAL PRIMARY KEY, name TEXT); INSERT INTO customers (id, name) VALUES (1, 'Jan'), (2, 'Tim');").expect("setup");
 
-    println!("Querying all customers...");
-    for customer in client.run(&GetCustomers).expect("query") {
-        println!("Got customer: {:?}", customer);
-    }
-
-    println!("Querying all customers another way...");
-    for customer in client.run(&GetCustomers2).expect("query") {
-        println!("Got customer: {:?}", customer);
-    }
-
-    println!("Searching for customers with name ending 'm'...");
-    for customer in client.run(&SearchCustomersByName("%m")).expect("query") {
-        println!("Got customer: {:?}", customer);
-    }
-
-    println!("Getting customer by id 1...");
-    let customer = client.run_one(&GetCustomer::by_id(1)).expect("query");
-    println!("Got customer: {:?}", customer);
+    let ok = match run_test(&mut client) {
+        Ok(_) => true,
+        Err(e) => {
+            eprintln!("Error: {e}");
+            false
+        }
+    };
 
     println!("Dropping table...");
     client.batch_execute("DROP TABLE customers;").expect("setup");
+
+    ok
 }
 
 fn main() {
     println!("Testing synchronous client...");
-    sync_main();
-    println!("Done.");
+    let ok = sync_main();
+    println!("Done{}.", if ok { "" } else { " (with errors)" });
 }
