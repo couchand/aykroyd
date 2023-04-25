4f21eba02b650aa561c06cbba4cba2afe78aed27
Andrew Dona-Couch <hi@andrewcou.ch>
2023-04-25 13:00:05 -0400

    update example to include database access

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,55 @@
-fn main() {
-    let migrations = akroyd_migrate::include_migrations!();
-    let repo = akroyd_migrate::embedded::EmbeddedRepo::load(&migrations).unwrap();
+use akroyd_migrate::*;
 
-    println!("{:?}", repo);
+static MIGRATIONS: embedded::EmbeddedMigrations = include_migrations!();
+
+#[cfg(feature = "sync")]
+fn main() {
+    try_main().unwrap()
+}
+
+#[cfg(feature = "sync")]
+fn try_main() -> Result<(), tokio_postgres::Error> {
+    use akroyd::sync_client::Client;
+
+    let embedded_repo = akroyd_migrate::embedded::EmbeddedRepo::load(&MIGRATIONS).unwrap();
+    println!("{:?}", embedded_repo);
+
+    let mut client = Client::connect(
+        "host=localhost user=akroyd_test password=akroyd_test",
+        tokio_postgres::NoTls,
+    )?;
+
+    println!("Loading from database...");
+    let database_repo = db::DatabaseRepo::from_sync_client(&mut client).unwrap();
+    println!("{database_repo:?}");
+
+    println!("Done.");
+    Ok(())
+}
+
+#[cfg(feature = "async")]
+#[tokio::main]
+async fn main() -> Result<(), tokio_postgres::Error> {
+    use akroyd::async_client::connect;
+
+    let embedded_repo = akroyd_migrate::embedded::EmbeddedRepo::load(&MIGRATIONS).unwrap();
+    println!("{:?}", embedded_repo);
+
+    let (mut client, conn) = connect(
+        "host=localhost user=akroyd_test password=akroyd_test",
+        tokio_postgres::NoTls,
+    ).await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("connection error: {e}");
+        }
+    });
+
+    println!("Loading from database...");
+    let database_repo = db::DatabaseRepo::from_async_client(&mut client).await.unwrap();
+    println!("{database_repo:?}");
+
+    println!("Done.");
+    Ok(())
 }
