0b18b83b1c9d639221f9b4e745f611da6dc53ed0
Andrew Dona-Couch <hi@andrewcou.ch>
2024-01-05 13:19:18 -0500

    text back in query

--- aykroyd-v2/src/lib.rs
+++ aykroyd-v2/src/lib.rs
@@ -3,7 +3,6 @@ pub mod combinator;
 pub mod error;
 pub mod query;
 pub mod row;
-pub mod text;
 
 pub mod mysql;
 pub mod postgres;
@@ -17,4 +16,3 @@ pub use combinator::*;
 pub use error::*;
 pub use query::*;
 pub use row::*;
-pub use text::*;
--- aykroyd-v2/src/query.rs
+++ aykroyd-v2/src/query.rs
@@ -1,12 +1,26 @@
-use super::{Client, QueryText, FromRow};
+use super::{Client, FromRow};
+
+pub trait QueryText {
+    fn query_text(&self) -> String;
+}
+
+pub trait StaticQueryText {
+    const QUERY_TEXT: &'static str;
+}
+
+impl<S: StaticQueryText> QueryText for S {
+    fn query_text(&self) -> String {
+        Self::QUERY_TEXT.into()
+    }
+}
 
 pub trait ToParams<C: Client>: Sync {
     fn to_params(&self) -> Vec<C::Param<'_>>;
 }
 
-pub trait Statement<C: Client>: ToParams<C> + QueryText + Sync {}
+pub trait Statement<C: Client>: QueryText + ToParams<C> + Sync {}
 
-pub trait Query<C: Client>: ToParams<C> + QueryText + Sync {
+pub trait Query<C: Client>: QueryText + ToParams<C> + Sync {
     type Row: for<'a> FromRow<C::Row<'a>>;
 }
 
