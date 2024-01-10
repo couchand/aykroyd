770704ffe5d8b2f0ca72b79070069ed625433ebd
Andrew Dona-Couch <hi@andrewcou.ch>
2024-01-09 22:52:25 -0500

    docs

--- aykroyd-v2/src/lib.rs
+++ aykroyd-v2/src/lib.rs
@@ -1,4 +1,31 @@
-//! Aykroyd: An opinionated micro-ORM.
+//! An opinionated micro-ORM for Rust.
+//!
+//! Database queries are represented by a plain Rust struct that implements
+//! either [`Statement`](query::Statement) or [`Query`](query::Query) (and
+//! maybe [`QueryOne`](query::QueryOne)).  The traits `Statement` and `Query`
+//! share two common parent traits:
+//!
+//! * [`QueryText`](query::QueryText), which gives access to the text of the
+//!   query, and
+//! * [`ToParams`](query::ToParams), which we can use to turn the struct into
+//!   database parameters.
+//!
+//! Using these together, a database client can prepare the text of a query
+//! and then run it on a database, passing in the required parameters.
+//!
+//! In addition, the `Query` trait has an associated type `Row` which must
+//! implement:
+//!
+//! * [`FromRow`](row::FromRow), to be deserialized from database rows.
+//!
+//! All of these traits can be derived automatically, such that usual
+//! gnarly database access code is reduced to simple struct definitions.
+//! These structs logically bind the query text to the input and output types.
+//!
+//! The binding is not magic, there is no verification against a database.
+//! `Query` and `Statement` implementations are an assertion by the developer,
+//! one that you would be wise to verify.  It is recommended to write a
+//! suite of automated tests which can be run against any database tier.
 
 pub mod client;
 pub mod combinator;
